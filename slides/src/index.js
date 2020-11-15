8a6a57e6d09e783f866fc77702b6221dc327f1b1
Julian Pasquale <julianagustin78@gmail.com>
2020-11-15 16:57:41 -0300

    Progress with class 3 slides

--- slides/src/index.js
+++ slides/src/index.js
@@ -2,17 +2,21 @@ import Reveal from 'reveal.js';
 
 // Reveal plugin.
 import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
+import RevealHighlight from 'reveal.js/plugin/highlight/highlight.js'
 
 // Reveal CSS files.
 import 'reveal.js/dist/reveal.css'
 import 'reveal.js/dist/theme/league.css'
+
+// Theme used for syntax highlighted code
+import 'reveal.js/plugin/highlight/monokai.css'
 
 // Custom shared CSS.
 import './index.css'
 
 window.onload = () => {
    let deck = new Reveal({
-      plugins: [ Markdown ]
+      plugins: [ Markdown, RevealHighlight ]
    });
    
    deck.initialize();
